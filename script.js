bd32b45d76c00c27895a513ba1a320f8cd577d9b
Axel <cymbalaxel@gmail.com>
2024-02-05 16:42:43 -0300

    se corrigio un error en el eventlistener que se estaba ejecutando la duncion

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+const addBook = document.getElementById("add-book-button");
+
+
+
+
+
 const myLibrary = [];
 
-function Book(){
-    //constructor
+function Book(title, author, pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
+
+
 function addBookToLibrary(){
-    
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+
+    const newBook = new Book(title, author, pages)
+
+    /*const myObject = {
+        title: title,
+        author: author,
+        pages: pages
+    }*/
+    myLibrary.push(newBook);
+    console.log(myLibrary)
 }
+
+addBook.addEventListener("click", addBookToLibrary);
