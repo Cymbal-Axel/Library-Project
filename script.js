5d2cc9bedac1589799bcb163bbf490e34508ad67
Axel <cymbalaxel@gmail.com>
2024-03-04 16:10:52 -0300

    se modifico el condicional que creaba los elementos del html, se lo puso en una nueva funcion llamada "addBookToLibrary" donde se le pasa el objeto por parametro. El condicional del if se ubico en donde se pushea el objeto nuevo creado. tambien se agrego una funcion al objeto para un boton de delete, y se crearon ID para el boton delet y para cada div nuevo creado. Tambien se agrego la funcion "deleteCard" y un addEventListener para el deleteButton

--- script.js
+++ script.js
@@ -1,17 +1,74 @@
 const addBook = document.getElementById("add-book-button");
 
+var bookCardDiv = document.getElementById("library-container");
+
+const addBookCardToLibraryDiv = (book) => {
+  
+  	        var newDiv = document.createElement("div");
+             newDiv.setAttribute("id", book.id); 
+
+            var newTitle = document.createElement("span");
+            var newH5 = document.createElement("h5");
+            var titleContent = document.createTextNode(`title:  ${book.title}`);
+
+            newH5.appendChild(titleContent);
+            newTitle.appendChild(newH5);
+            newDiv.appendChild(newTitle);
+
+            var newAuthor = document.createElement("span");
+            var newH5 = document.createElement("h5");
+            var authorContent = document.createTextNode(`Author:  ${book.author}`);
+
+            newH5.appendChild(authorContent);
+            newAuthor.appendChild(newH5);
+            newDiv.appendChild(newAuthor);
+
+            var newPages = document.createElement("span");
+            var newH5 = document.createElement("h5");
+            var pagesContent = document.createTextNode(`Pages:  ${book.pages}`);
+
+            newH5.appendChild(pagesContent);
+            newPages.appendChild(newH5);
+            newDiv.appendChild(newPages);
+    
+            bookCardDiv.appendChild(newDiv);
+  
+}
 
 
-
+ const deleteCard = (id) => {
+    console.log(id)
+}
 
 const myLibrary = [];
 
 function Book(title, author, pages){
+    bookId =  crypto.randomUUID()
+    this.id = bookId
     this.title = title;
     this.author = author;
     this.pages = pages;
-}
+    this.button = function(){
+        var createDeleteButton = document.createElement("button");
+        var buttonText = document.createTextNode("Delete");
 
+        createDeleteButton.setAttribute("id", "delete-button"); 
+
+         var deleteButton = document.getElementById("delete-button");
+
+         createDeleteButton.addEventListener("click", function(){
+             deleteCard(bookId.saraza);
+         }, false);
+
+
+
+        createDeleteButton.appendChild(buttonText);
+        bookCardDiv.appendChild(createDeleteButton);
+
+
+    }
+
+}
 
 
 function addBookToLibrary(){
@@ -22,6 +79,8 @@ function addBookToLibrary(){
     if(title || author || pages){
         const newBook = new Book(title, author, pages)
         myLibrary.push(newBook);
+        addBookCardToLibraryDiv(newBook);
+        newBook.button()
     }
 
 
@@ -33,31 +92,8 @@ function addBookToLibrary(){
         input.value = '';
     });
 
-    if(myLibrary.length >= 1){
-        myLibrary.forEach(object => {
-            var displayBooks = document.getElementById("library-container");
-                    
-            var newDiv = document.createElement("div");
-            var newTitle = document.createElement("span");
-            var newH3 = document.createElement("h3");
-    
-            var titleContent = document.createTextNode(object.title);
-
-                newH3.appendChild(titleContent);
-
-                newTitle.appendChild(newH3);
-    
-                newDiv.appendChild(newTitle);
-    
-                displayBooks.appendChild(newDiv);
-            
-
-        })
-    }
-
 }
 
-//iterar sobre el array de objetos y sobre el elemento del objeto acceder a la informacion, por ejemplo: myLibrary[0].title/myLibrary[0].pages/myLibrary[0].author
 
 
 addBook.addEventListener("click", addBookToLibrary);
